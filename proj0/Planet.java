f74acb4bfd6e60bb3ebf83e7fb5af274dbde3416
whale-withme <3052748978@qqcom>
2024-03-25 20:28:17 +0800

    public members in class

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,10 +1,10 @@
 public class Planet {
-    private double xxPos;
-    private double yyPos;
-    private double xxVel;
-    private double yyVel;
-    private double mass;
-    private String imgFileName;
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
 
     /* Constor */
     public Planet(double xP, double yP, double xV, double yV, double m, String img){
